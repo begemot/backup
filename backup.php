a21db07a067ffcf7b466b5448b2c14dd20197481
begemot <scott2to@gmail.com>
2023-06-29 00:26:27 +0300

    Список директорий для бекапа отдельным файлом. Каждая строка - одна директория.

--- backup.php
+++ backup.php
@@ -8,7 +8,10 @@ $dirsForBackup = [
 '/home/php_mysql_dump'
 ];
 
-
+ 
+$dirsForBackup = file('./exclude.data', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+print_r($dirsForBackup);
+return;
 
 foreach ($dirsForBackup as $path){
 $command ="rsync -avhb --exclude '*/protected/runtime/*' --exclude '*.log' --delete --backup-dir=/bkp/bacUpOld/c_$(date +%d.%m.%Y_%H:%M) $path /bkp";
