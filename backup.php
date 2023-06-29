dff2f4742e724a438e3dc3504f32581a07df12d7
begemot <scott2to@gmail.com>
2023-06-29 13:24:17 +0300

    Исправил опечатку и изменил имя файла с директориями.

--- backup.php
+++ backup.php
@@ -9,12 +9,13 @@ $dirsForBackup = [
 ];
 
  
-$dirsForBackup = file('./exclude.data', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+$dirsForBackup = file('./backuplist.data', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 print_r($dirsForBackup);
-return;
+
+
 
 foreach ($dirsForBackup as $path){
-$command ="rsync -avhb --exclude '*/protected/runtime/*' --exclude '*.log' --delete --backup-dir=/bkp/bacUpOld/c_$(date +%d.%m.%Y_%H:%M) $path /bkp";
+$command ="rsync -avhb --exclude '*/protected/runtime/*' --exclude '*.log' --delete --backup-dir=./bkp/backUpOld/c_$(date +%d.%m.%Y_%H:%M) $path ./bkp";
 echo exec ($command);
 }
 
